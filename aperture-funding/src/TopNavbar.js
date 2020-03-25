1b281ed02c089572217b1e2f496d4179d2d3a695
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-03-24 21:46:33 -0600

    Added the navbar component

--- aperture-funding/src/TopNavbar.js
+++ aperture-funding/src/TopNavbar.js
@@ -11,15 +11,25 @@ import Row from "react-bootstrap/Row";
 
 const TopNavbar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Aperture Science</Navbar.Brand>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">
+                <img
+                    alt="aperture science logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Aperture_Science_Logo.svg/1200px-Aperture_Science_Logo.svg.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                Aperture Science
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/login">Log in</Nav.Link>
-                    <Nav.Link href="/signup">Sign up</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-end" style={{ width: "100%" }}>
+                        <Nav.Link href="/project">Project (Template)</Nav.Link>
+                        <Nav.Link href="/login">Log in</Nav.Link>
+                        <Button variant="outline-info" href="/signup">Sign up</Button>
+                    </Nav>
+                </Navbar.Collapse>
         </Navbar>
     );
 }
