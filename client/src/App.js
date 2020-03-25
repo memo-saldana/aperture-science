4a2f4507c5d5cbca98a9fb7aec379dce82482c5c
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-03-24 20:42:20 -0600

    Started the template for the projects

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,28 @@ import Login from './Login';
 import React from "react";
 import Signup from './Signup';
 import Themer from './ThemeSetter';
+import ProjectView from './ProjectView';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import TopNavbar from './TopNavbar';
 
 function App() {
   return (
     <Router>
       <Themer />
+      <TopNavbar />
       <Switch>
+<<<<<<< HEAD:client/src/App.js
         <Route exact path="/login" component={Login}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/recovery-email" component={AccountEmail} />
         <Route exact path="/recovery-password" component={AccountPassword} />
+=======
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/recovery-email" component={AccountEmail} />
+        <Route path="/recovery-password" component={AccountPassword} />
+        <Route path="/project" component={ProjectView} />
+>>>>>>> Started the template for the projects:aperture-funding/src/App.js
       </Switch>
     </Router>
   );
